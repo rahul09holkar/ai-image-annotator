0458c6633e98d325bef122e35f53d3e1774c47d8
parishparab <parikshit.parab09@gmail.com>
2025-03-14 11:13:55 +0530

    added saving functionality

--- src/views/EditorView/TopNavigationBar/ExporterFunction/ExporterFunction.tsx
+++ src/views/EditorView/TopNavigationBar/ExporterFunction/ExporterFunction.tsx
@@ -1,20 +1,82 @@
 import { connect } from "react-redux";
 import { AppState } from "src/store";
 import { COCOExporter } from "../../../../logic/export/polygon/COCOExporter";
+import { useState } from "react";
 const ExporterFunction: React.FC = () => {
+  const [isSaved, setIsSaved] = useState<boolean>(false);
+
   const style: React.CSSProperties = {
     top: 35,
     cursor: "pointer",
   };
+  const savedStyle: React.CSSProperties = {
+    top: 35,
+    cursor: "pointer",
+    color: "#5cb85c",
+    fontWeight: "bold",
+  };
 
-  const onAccept = () => {
-    const response = COCOExporter.returnJson();
+  const postTheData = async (
+    token: string,
+    notationData: any,
+    image_name: string,
+    type: string
+  ) => {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("Authorization", `Bearer ${token}`);
+
+    const raw = JSON.stringify({
+      image_category: type,
+      image_name: image_name,
+      json_data: notationData,
+    });
+
+    const requestOptions: RequestInit = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow" as RequestRedirect,
+    };
+
+    const response = await (
+      await fetch(
+        "http://api-dev.adonhaircare.com/v1/batch/saveAnnotation",
+        requestOptions
+      )
+    ).json();
+    return response;
+  };
+
+  const onAccept = async () => {
+    const notationData = COCOExporter.returnJson();
     const params = new URLSearchParams(window.location.search);
     const encodedData = params.get("d");
     const decodedString = atob(encodedData);
     const jsonData = JSON.parse(decodedString);
-    debugger
+    const response = await postTheData(
+      jsonData.token,
+      notationData,
+      jsonData.name,
+      jsonData.imageType
+    );
+    if (response.code === 200) {
+      setIsSaved(true);
+
+      setTimeout(() => {
+        setIsSaved(false);
+      }, 5000);
+    }
   };
+
+  if (isSaved) {
+    return (
+      <div style={savedStyle}>
+        <div className="Marker" />
+        Saved
+      </div>
+    );
+  }
   return (
     <div
       style={style}
